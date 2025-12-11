34ce3b6a8f8edf3a9b3490394f1ef0c916b2ba90
PNadal260 <pau.nadal@estudiant.enti.cat>
2025-12-11 10:20:29 +0100

    creado archivo board.cpp y board.h

--- Ajedrez/Ajedrez/main.cpp
+++ Ajedrez/Ajedrez/main.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
-
-const int TAMAÑO  = 8;
+#include "board.h"
 
 int main(){
-	char tablero[TAMAÑO][TAMAÑO];
-	for (int i = 0; i < TAMAÑO; i++) {
-		for (int j = 0; j < TAMAÑO; j++)
-		{
-			tablero[i][j] = '*';
-		}
-	}
-	for (int i = -1, k = 8; i < TAMAÑO; i++,k--) {
-		if (k == 8)
-			std::cout << "  ";
-		else
-			std::cout << k + 1 << ' ';
-		for (int j = 0; j < TAMAÑO; j++)
-		{	
-			if (i == -1)
-				std::cout << j + 1 << ' ';
-			else
-				std::cout << tablero[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
+	board();
 	return 0;
 }
