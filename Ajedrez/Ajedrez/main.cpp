fa05b8f424af2417974d5f42cf7dc62528c74884
XavierBou <xavier.bou@estudiant.enti.cat>
2025-12-11 09:56:17 +0100

    tablero primera parte

--- Ajedrez/Ajedrez/main.cpp
+++ Ajedrez/Ajedrez/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
-void main() {
-	std::cout << "Hello Mundo";
-}
+const int TAMAÑO  = 8;
+
+int main(){
+	
+	char tablero[TAMAÑO][TAMAÑO];
+
+	for (int i = 0; i < TAMAÑO; i++) {
+		for (int j = 0; j < TAMAÑO; j++)
+		{
+
+			tablero[i][j] = '*';
+
+		}
+	}
+		for (int i = 0; i < TAMAÑO; i++) {
+			for (int j = 0; j < TAMAÑO; j++)
+			{	
+				std::cout << tablero[i][j] << " ";
+			}
+			std::cout << std::endl;
+		}
+		return 0;
+	}
