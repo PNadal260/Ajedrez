febb7bb78b91b08f065ab0914ac51e774c0a761e
PNadal260 <pau.nadal@estudiant.enti.cat>
2025-12-11 10:06:46 +0100

    coordenadas tablero

--- Ajedrez/Ajedrez/main.cpp
+++ Ajedrez/Ajedrez/main.cpp
@@ -3,23 +3,26 @@
 const int TAMAÑO  = 8;
 
 int main(){
-	
 	char tablero[TAMAÑO][TAMAÑO];
-
 	for (int i = 0; i < TAMAÑO; i++) {
 		for (int j = 0; j < TAMAÑO; j++)
 		{
-
 			tablero[i][j] = '*';
-
 		}
 	}
-		for (int i = 0; i < TAMAÑO; i++) {
-			for (int j = 0; j < TAMAÑO; j++)
-			{	
+	for (int i = -1, k = 8; i < TAMAÑO; i++,k--) {
+		if (k == 8)
+			std::cout << "  ";
+		else
+			std::cout << k + 1 << ' ';
+		for (int j = 0; j < TAMAÑO; j++)
+		{	
+			if (i == -1)
+				std::cout << j + 1 << ' ';
+			else
 				std::cout << tablero[i][j] << " ";
-			}
-			std::cout << std::endl;
 		}
-		return 0;
+		std::cout << std::endl;
 	}
+	return 0;
+}
